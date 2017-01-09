7f00226522e183f5e164e328aa180f2c4c0a64fb
Virgil Chereches <virgil.chereches@gmx.net>
2017-01-09 16:26:05 +0200

    Trying to fix rspec test cases

--- spec/classes/node_exporter_spec.rb
+++ spec/classes/node_exporter_spec.rb
@@ -1,30 +1,23 @@
 require 'spec_helper'
 
 describe 'prometheus::node_exporter' do
-  let :default_facts do
-    {
-      'operatingsystem' => 'CentOS',
-      'osfamily' => 'Redhat',
-      'operatingsystemrelease' => '7.0',
-      'architecture' => 'amd64',
-      'kernel' => 'Linux'
-    }
-  end
+  on_supported_os.each do |os, facts|
+    context "on #{os}" do
+      let(:facts) do
+        facts
+      end
 
-  context 'uses the correct binary path for version' do
-    let(:facts) { default_facts }
-
-    let(:params) do 
-      {
-        version => '0.13.0',
-        arch => 'amd64',
-        os => 'linux'
-      }
-    end
-
-    it do
-      is_expected.to contain_file('/usr/local/bin/node_exporter').with('target' => '/opt/staging/node_exporter-0.13.0.linux-amd64/node_exporter')
+    context 'with version specified' do
+      let(:params) do 
+        {
+          version: '0.13.0',
+          arch: 'amd64',
+          os: 'linux'
+        }
+      end
+      describe 'install correct binary' do
+        it { is_expected.to contain_file('/usr/local/bin/node_exporter').with('target' => '/opt/staging/node_exporter-0.13.0.linux-amd64/node_exporter') }
+      end
     end
   end
-
 end
