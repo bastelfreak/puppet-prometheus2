281b998b9a53504ecbf946053d93051fa7722015
Eivin Hatvik <eivin.hatvik@oracle.com>
2018-11-07 12:37:00 +0100

    Add MacOS support

--- spec/defines/daemon_spec.rb
+++ spec/defines/daemon_spec.rb
@@ -205,17 +205,17 @@ describe 'prometheus::daemon' do
               is_expected.to contain_file('/etc/default/smurf_exporter').with(
                 'mode'    => '0644',
                 'owner'   => 'root',
-                'group'   => 'root'
+                'group'   => '0'
               ).with_content(
                 %r{SOMEVAR="42"\n}
               )
             }
           elsif ['centos-6-x86_64', 'redhat-6-x86_64', 'centos-7-x86_64', 'redhat-7-x86_64'].include?(os)
             it {
-              is_expected.to contain_file('/etc/sysconfig/smurf_exporter').with(
+          is_expected.to contain_file('/etc/sysconfig/smurf_exporter').with(
                 'mode'    => '0644',
                 'owner'   => 'root',
-                'group'   => 'root'
+                'group'   => '0'
               ).with_content(
                 %r{SOMEVAR="42"\n}
               )
